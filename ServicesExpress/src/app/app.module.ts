7e209b7af17c5b8e268aee982e12ff6a4dd2a526
Leonardo <josel.luzt@ecci.edu.co>
2020-07-24 18:05:17 -0500

    Begin proyect

--- ServicesExpress/src/app/app.module.ts
+++ ServicesExpress/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderMain } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMain
   ],
   imports: [
     BrowserModule
---
+++ ServicesExpress/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'headermain',
+    templateUrl: './header.component.html' 
+})
+export class HeaderMain{
+
+    constructor(){
+
+    }
+}
